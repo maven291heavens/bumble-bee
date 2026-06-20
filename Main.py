b11a8c29b51323a61d231e3ba2b3cecf1e1d9ba0
maven291heavens <94922200+maven291heavens@users.noreply.github.com>
2026-06-20 18:47:34 +0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,31 +1,60 @@
-matrix = [
-    [1, 2, 3]
-    [4, 5, 6]
-    [7, 8, 9]
-    ]
+import pgzrun
+from random import randint
+
+WIDTH = 600
+HEIGHT = 500
+
+score = 0
+game_over = False
+
+bee = Actor("bee")
+bee.pos = 100,100
+
+flower = Actor("flower")
+flower.pos = 200,200
+
+def draw():
+    screen.blit("bg", (0,0))
+    flower.draw()
+    bee.draw()
+    screen.draw.text("Score: " + str(score), color="white", topleft=(10,10))
+
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Time's up! Your Final Score: " + str(score), midtop=(WIDTH/ 2,10), fontsize=40, color="red")
 
 
-print matrix([0][0])
-print matrix([1][2])
+def place_flower():
+    flower.x = randint(70, (WIDTH-70))
+    flower.y = randint(70, (HEIGHT-70))
 
-matrix[2][1] = 88
-print(matrix)
+def time_up():
+    global game_over
+    game_over = True
 
-for row in matrix:
-    for col in row:
-        print(col, end = " ")
-    print(" ")
+def update():
+    global score
+     
+    if keyboard.d:
+        bee.x = bee.x - 2
+    
+    if keyboard.a:
+        bee.x = bee.x + 2
 
-rows = int(input("Enter number of rows: "))
-cols = int(input("Enter number of coloumns"))
-matrix = []
-for i in range(rows):
-    row = []
-    for j in range(cols): 
-        val = int(input("Enter element at {}, {}: ").format(i,j))
-        row.append(val)
-    matrix.append(row)
+    if keyboard.s:
+        bee.y = bee.y - 2
 
-print("Your 2D List: ")
-for row in matrix:
-    print(row)
+    if keyboard.w:
+        bee.y = bee.y + 2
+
+    flower_collected = bee.colliderect(flower)
+
+    if flower_collected:
+        score = score + 10
+        place_flower()
+
+clock.schedule(time_up, 60.0)
+
+
+
+pgzrun.go()
